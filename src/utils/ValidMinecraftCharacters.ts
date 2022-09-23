b06751458b21ac624f043dd3c33fc63067787284
Lily Smart <hello@neyoa.me>
2022-09-23 13:13:44 +0100

    Improve regex

--- src/utils/ValidMinecraftCharacters.ts
+++ src/utils/ValidMinecraftCharacters.ts
@@ -1,4 +1,4 @@
-const regex = /[^\w!@#$%^&*()+\-=[\]{};':"\\|,.<>/? ]/g
+const regex = /[^\p{Letter}\p{Number}\p{Punctuation}\p{Space_Separator}\p{Math_Symbol}\p{Currency_Symbol}\p{Modifier_Symbol}\u2700-\u27BF]/gu
 
 export function cleanString(str: string): string {
   return str.normalize().replace(regex, '')
